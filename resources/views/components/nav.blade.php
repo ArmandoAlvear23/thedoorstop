43cb1ef85ab675f945b4dad038aae3325fa8c321
Armando Alvear <armando.alvear01@outlook.com>
2023-10-05 18:32:16 -0500

    add responsiveness to navbar

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,13 +1,13 @@
 <header>
-    <div class="pt-2 pb px-4 flex justify-between items-center bg-gray-100">
+    <div class="py-px px-6 flex justify-between items-center bg-primary sm:bg-gray-100 lg:px-56">
         <div>
             <a href="/">
-                <img src="{{ asset("images/the-door-stop-logo.png") }}" alt="The Door Stop" class="w-24">
+                <img src="{{ asset("images/the-door-stop-logo.png") }}" alt="The Door Stop" class="w-28">
             </a>
         </div>
         <div>
-            <div class="px-4 cursor-pointer">
-                <i class="fa-solid fa-bars fa-xl sm:hidden"></i>
+            <div class="py-2 px-4 rounded cursor-pointer sm:hidden border-2">
+                <i class="fa-solid fa-bars fa-xl text-white"></i>
             </div>
             <div  class="text-right hidden sm:block">
                 <a href="tel:956-574-9071" class="font-bold text-red-600"><span><i class="fa-solid fa-phone fa-shake"></i> (956) 574-9071</span></a>
@@ -19,8 +19,8 @@
             </div>
         </div>
     </div>
-    <nav class="bg-primary py-4 px-6 hidden sm:block">
-        <ul class="flex space-x-7">
+    <nav class="bg-primary py-4 px-6 hidden sm:block shadow-md">
+        <ul class="flex space-x-7 lg:justify-center">
             <li class="{{ Request::is('/doors/modern') ? 'border-b-2 border-secondary' : 'border-b-2 border-primary' }} hover:border-secondary"><a href="/doors/modern" class="text-white uppercase">Modern</a></li>
             <li class="{{ Request::is('/doors/traditional') ? 'border-b-2 border-secondary' : 'border-b-2 border-primary' }} hover:border-secondary"><a href="/doors/traditional" class="text-white uppercase">Traditional</a></li>
             <li class="{{ Request::is('/doors/rustic') ? 'border-b-2 border-secondary' : 'border-b-2 border-primary' }} hover:border-secondary"><a href="/doors/rustic" class="text-white uppercase">Rustic</a></li>
