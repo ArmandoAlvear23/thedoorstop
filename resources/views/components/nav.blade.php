c94f248635afc68e2436fe357b96aec11453c876
Armando Alvear <armando.alvear01@outlook.com>
2023-10-05 17:19:04 -0500

    add bottom border to nav items and hamburger for small size screens

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,27 +1,32 @@
 <header>
     <div class="pt-2 pb px-4 flex justify-between items-center bg-gray-100">
-        <div class="w-24">
-            <a href="/" class="">
-                <img src="{{ asset("images/the-door-stop-logo.png") }}" alt="The Door Stop">
+        <div>
+            <a href="/">
+                <img src="{{ asset("images/the-door-stop-logo.png") }}" alt="The Door Stop" class="w-24">
             </a>
         </div>
-        <div class="text-right">
-            <a href="tel:956-574-9071" class="font-bold text-red-600"><span><i class="fa-solid fa-phone fa-shake"></i> (956) 574-9071</span></a>
-            <ul class="text-xs text-gray-700">
-                <li>Mon-Fri: 8:30a-5:30p</li>
-                <li>Saturday: 10a-2p</li>
-                <li>Sunday: Closed</li>
-            </ul>
+        <div>
+            <div class="px-4 cursor-pointer">
+                <i class="fa-solid fa-bars fa-xl sm:hidden"></i>
+            </div>
+            <div  class="text-right hidden sm:block">
+                <a href="tel:956-574-9071" class="font-bold text-red-600"><span><i class="fa-solid fa-phone fa-shake"></i> (956) 574-9071</span></a>
+                <ul class="text-xs text-gray-700">
+                    <li>Mon-Fri: 8:30a-5:30p</li>
+                    <li>Saturday: 10a-2p</li>
+                    <li>Sunday: Closed</li>
+                </ul>
+            </div>
         </div>
     </div>
-    <nav class="bg-primary py-4 px-6">
+    <nav class="bg-primary py-4 px-6 hidden sm:block">
         <ul class="flex space-x-7">
-            <li class="border-b-2 border-secondary"><a href="#" class="text-white uppercase">Modern</a></li>
-            <li class="hover:border-b"><a href="#" class="text-white uppercase">Traditional</a></li>
-            <li class="hover:border-b"><a href="#" class="text-white uppercase">Rustic</a></li>
-            <li class="hover:border-b"><a href="#" class="text-white uppercase">Craftsman</a></li>
-            <li class="hover:border-b"><a href="#" class="text-white uppercase">About</a></li>
-            <li class="hover:border-b"><a href="#" class="text-white uppercase">Contact</a></li>
+            <li class="{{ Request::is('/doors/modern') ? 'border-b-2 border-secondary' : 'border-b-2 border-primary' }} hover:border-secondary"><a href="/doors/modern" class="text-white uppercase">Modern</a></li>
+            <li class="{{ Request::is('/doors/traditional') ? 'border-b-2 border-secondary' : 'border-b-2 border-primary' }} hover:border-secondary"><a href="/doors/traditional" class="text-white uppercase">Traditional</a></li>
+            <li class="{{ Request::is('/doors/rustic') ? 'border-b-2 border-secondary' : 'border-b-2 border-primary' }} hover:border-secondary"><a href="/doors/rustic" class="text-white uppercase">Rustic</a></li>
+            <li class="{{ Request::is('/doors/rustic') ? 'border-b-2 border-secondary' : 'border-b-2 border-primary' }} hover:border-secondary"><a href="doors/craftsman" class="text-white uppercase">Craftsman</a></li>
+            <li class="{{ Request::is('/about') ? 'border-b-2 border-secondary' : 'border-b-2 border-primary' }} hover:border-secondary"><a href="/about" class="text-white uppercase">About</a></li>
+            <li class="{{ Request::is('/contact') ? 'border-b-2 border-secondary' : 'border-b-2 border-primary' }} hover:border-secondary"><a href="/contact" class="text-white uppercase">Contact</a></li>
         </ul>
     </nav>
 </header>
