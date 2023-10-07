708e266f926c748242c7c4752081f6093c0a32ef
Armando Alvear <armando.alvear01@outlook.com>
2023-10-07 01:13:41 -0500

    add about page and image

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,10 @@
 <x-layout> 
+    <img src="{{ asset('images/tds-store-front.png') }}" class="shadow-lg" alt="The Door Stop store front"/>
     <x-padding-wrapper>
-        <h1>About page</h1>
+        <div class="mt-8">
+            <h1>Better Solutions For Your Business</h1>
+        <p>In business for more than 30 years. Specializing in residential fiberglass doors, with affordable prices. A wide variety of exterior doors in stock. We are located behind Burton Auto Supplies on International Blvd. Brownsville, Tx</p>
+        </div>
+        
     </x-padding-wrapper>
 </x-layout>
