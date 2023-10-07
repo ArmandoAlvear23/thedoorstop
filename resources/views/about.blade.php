b16378c8751c26f0116a4cbffeff43f9dc334749
Armando Alvear <armando.alvear01@outlook.com>
2023-10-07 12:43:19 -0500

    add about page content and styling

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,8 +2,40 @@
     <img src="{{ asset('images/tds-store-front.png') }}" class="shadow-lg" alt="The Door Stop store front"/>
     <x-padding-wrapper>
         <div class="mt-8">
-            <h1>Better Solutions For Your Business</h1>
-        <p>In business for more than 30 years. Specializing in residential fiberglass doors, with affordable prices. A wide variety of exterior doors in stock. We are located behind Burton Auto Supplies on International Blvd. Brownsville, Tx</p>
+            <!-- Introduction -->
+            <h1 class="text-primary font-bold text-2xl mb-2">Where Quality Doors Open New Opportunities</h1>
+            <p class=" text-gray-600 text-lg px-1 mb-6">At The Door Stop, we believe that doors are more than just functional fixtures; they are the gateway to your dreams, the entrance to your haven, and the first impression of your space. That's why we are passionate about providing you with the finest doors that not only enhance the security and aesthetics of your property but also reflect your unique style and personality.</p>
+
+            <!-- Our Story -->
+            <h3 class="text-primary font-bold text-2xl mb-2">Our Story</h3>
+            <p class="text-gray-600 text-lg px-1 mb-6">Founded in 1993 in Brownsville, TX, The Door Stop has been a trusted name in the door industry for over 30 years. Our journey began with a simple mission - to provide our customers in the Rio Grande Valley with top-notch doors that blend durability, style, and value. Over the years, we've grown and evolved, but our commitment to excellence remains unwavering.</p>
+
+            <!-- What Sets Us Apart -->
+            <h3 class="text-primary font-bold text-2xl mb-2">What Sets Us Apart</h3>
+            <ol class="list-decimal px-6 mb-6">
+                <li class="text-gray-600 text-lg mb-2">
+                    <strong>Quality Craftsmanship:</strong> We take pride in sourcing and crafting doors that are built to last. Each door in our collection undergoes rigorous quality checks to ensure it meets our high standards.
+                </li>
+                <li class="text-gray-600 text-lg mb-2">
+                    <strong>Diverse Selection:</strong> Whether you're looking for elegant entry doors, secure interior doors, stylish patio doors, or custom-made designs, we have a wide range of options to suit your needs.
+                </li>
+                <li class="text-gray-600 text-lg mb-2">
+                    <strong>Expert Guidance:</strong> Our team of knowledgeable professionals is here to guide you through the selection process, offering expert advice and helping you find the perfect door that complements your space and budget.
+                </li>
+                <li class="text-gray-600 text-lg mb-2">
+                    <strong>Customer-Centric Approach:</strong> Your satisfaction is our top priority. We go the extra mile to ensure you have a seamless shopping experience, from browsing our catalog to delivery and installation.
+                </li>
+            </ol>
+
+            <!-- Our Vision -->
+            <h3 class="text-primary font-bold text-2xl mb-2">Our Vision</h3>
+            <p class="text-gray-600 text-lg px-1 mb-2">We envision a world where every doorway is an opportunity for beauty, security, and individuality. Our doors are not just functional; they are a statement of your lifestyle, values, and aspirations.</p>
+            <p class="text-gray-600 text-lg px-1 mb-2">Whether you're a homeowner, contractor, or interior designer, we invite you to explore our extensive range of doors, backed by our commitment to quality and excellence.</p>
+            <p class="text-gray-600 text-lg px-1 mb-6">Thank you for considering The Door Stop as your trusted partner in finding the perfect doors to open new possibilities for your space. We look forward to being a part of your journey.</p>
+
+            <!-- Slogan -->
+            <h3 class="text-xl text-center font-light text-gray-500 mb-8">Discover the door that opens to a world of possibilities with The Door Stop</h3>
+
         </div>
         
     </x-padding-wrapper>
