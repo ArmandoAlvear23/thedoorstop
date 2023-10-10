694c4d64c38a61d12dfa24b13b16bd5b860750e8
Armando Alvear <armando.alvear01@outlook.com>
2023-10-09 21:07:22 -0500

    add category seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Door;
 use App\Models\Message;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,9 +15,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(5)->create();
+        //\App\Models\User::factory(5)->create();
 
+        // Create test message
         Message::factory(50)->create();
+
+        // Create door categories
+        Category::create([
+            'name' => 'interior',
+        ]);
+        Category::create([
+            'name' => 'exterior',
+        ]);
+        Category::create([
+            'name' => 'modern',
+        ]);
+        Category::create([
+            'name' => 'traditional',
+        ]);
+        Category::create([
+            'name' => 'rustic',
+        ]);
+        Category::create([
+            'name' => 'craftsman',
+        ]);
+
+        // Create doors
+        Door::create([
+            'name' => '',
+            'sku' => '',
+            'img_location' => '',
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
