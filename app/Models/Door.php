4f7943fb6f775f4010aaedd25684dc12c22a5281
Armando Alvear <armando.alvear01@outlook.com>
2023-10-18 10:47:55 -0500

    add scopefilter and categories relationship function

--- app/Models/Door.php
+++ app/Models/Door.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Door extends Model
 {
     use HasFactory;
+
+    public function scopeFilter($query, array $filters) {
+        if($filters['category'] ?? false) {
+            $query->where('');
+        }
+    }
+
+    public function categories(): BelongsToMany {
+        return $this->belongsToMany(Category::class)->withTimestamps();
+    }
 }
