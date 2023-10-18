4d6463ca652b1db8f539b735b36a56405d8b0004
Armando Alvear <armando.alvear01@outlook.com>
2023-10-18 10:46:36 -0500

    add relationships to doors and classification

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function scopeFilter($query, array $filters) {
+
+        if($filters['search'] ?? false) {
+            $query->where('name', 'like', '%' . request('category') . '%');
+        }
+    }
+
+    public function doors(): BelongsToMany
+    {
+        return $this->belongsToMany(Door::class)->withTimestamps();
+    }
+
+    public function classification(): BelongsTo
+    {
+        return $this->belongsTo(Classification::class);
+    }
 }
