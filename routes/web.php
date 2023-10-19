96df284eb1d775f6c5b8aec7b59d8e84318db520
Armando Alvear <armando.alvear01@outlook.com>
2023-10-19 16:41:13 -0500

    organize view folder structure and modify web route file with new routes

--- routes/web.php
+++ routes/web.php
@@ -4,34 +4,22 @@ use App\Models\Door;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MessageController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    $door = Door::find(1);
-        $door->categories()->sync([1, 2, 3]);
-        echo('attached categories!');
-    return view('index');
+    return view('pages.index');
 });
 
 Route::get('/contact', function() {
-    return view('contact');
+    return view('pages.contact');
 });
 
 Route::get('/about', function() {
-    return view('about');
+    return view('pages.about');
 });
 
 // Get All Doors That Belong To Passed In Category
 Route::get('/doors/{category}', [DoorController::class, 'index']);
+
+Route::post('/doors', [DoorController::class, 'store']);
 
 // All Messages
 Route::get('/internal/messages', [MessageController::class, 'index']);
@@ -39,4 +27,4 @@ Route::get('/internal/messages', [MessageController::class, 'index']);
 // Single Message
 Route::get('/internal/messages/{listing}', [MessageController::class, 'show']);
 
-Route::post('/doors', [DoorController::class, 'store']);
+
