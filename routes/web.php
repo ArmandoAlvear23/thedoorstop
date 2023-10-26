0febecd44749d73411da04e3c07bce5cfab561f9
Armando Alvear <armando.alvear01@outlook.com>
2023-10-26 04:05:17 -0500

    add door routes, comments, and reorganize routes

--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,83 @@
 <?php
 
-use App\Models\Door;
-use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DoorController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ClassificationController;
 
+////////////////////////////////
+// Navigation Routes
+////////////////////////////////
+
+// Get Home View
 Route::get('/', function () {
     return view('pages.index');
 });
 
-Route::get('/contact', function() {
-    return view('pages.contact');
-});
-
+//Get About View
 Route::get('/about', function() {
     return view('pages.about');
 });
 
-// Get All Doors That Belong To Passed In Category
-Route::get('/doors/{category}', [DoorController::class, 'index']);
+// Get Contact View
+Route::get('/contact', function() {
+    return view('pages.contact');
+});
 
-Route::post('/doors', [DoorController::class, 'store']);
+////////////////////////////////
+// Door Routes
+////////////////////////////////
 
-// All Messages
-Route::get('/internal/messages', [MessageController::class, 'index']);
+// Get All Doors View
+Route::get('/doors', [DoorController::class, 'index']);
 
-// Store Message
-Route::post('/internal/messages', [MessageController::class, 'store'])->name('storeMessage');
+// Get Create Door View
+Route::get('/internal/doors/create', [DoorController::class, 'create'])->name('createDoor');
 
-// Single Message
-Route::get('/internal/messages/{listing}', [MessageController::class, 'show']);
+// Store New Door
+Route::post('/internal/doors', [DoorController::class, 'store'])->name('storeDoor');
 
-// Get Categories Index Page (Show all Classifications and their respective categories)
+// Get Edit Door View
+Route::get('/internal/doors/{door}/edit', [DoorController::class, 'edit'])->name('editDoor');
+
+// Update Door
+Route::put('/internal/doors/{door}', [DoorController::class, 'update'])->name('updateDoor');
+
+// Delete Door
+Route::delete('/internal/doors/{door}', [DoorController::class, 'destroy'])->name('destroyDoor');
+
+// Get Manage Door View
+Route::get('/internal/doors/{door}/manage', [DoorController::class, 'manage'])->name('manageDoor');
+
+// Get Show Door View
+Route::get('/doors/{door}', [DoorController::class, 'show'])->name('showDoor');
+
+////////////////////////////////
+// Classification Routes
+////////////////////////////////
+
+// Get Create Classification View
+Route::get('/internal/door/classifications/create/', [ClassificationController::class, 'create'])->name('createClassification');
+
+// Store New Classification
+Route::post('/internal/door/classifications', [ClassificationController::class, 'store'])->name('storeClassification');
+
+// Get Edit Classification View
+Route::get('/internal/door/classifications/{classification}/edit', [ClassificationController::class, 'edit'])->name('editClassification');
+
+// Update Classification
+Route::put('/internal/door/classifications/{classification}', [ClassificationController::class, 'update'])->name('updateClassification');
+
+// Delete Classification
+Route::delete('/internal/door/classifications/{classification}', [ClassificationController::class, 'destroy'])->name('destroyClassification');
+
+
+////////////////////////////////
+// Category Routes
+////////////////////////////////
+
+// Get All Categories View
 Route::get('/internal/door/categories', [CategoryController::class, 'index']);
 
 // Get Create Category View
@@ -49,20 +92,18 @@ Route::get('/internal/door/categories/{category}/edit', [CategoryController::cla
 // Update Category
 Route::put('/internal/door/categories/{category}', [CategoryController::class, 'update'])->name('updateCategory');
 
-// Delete Category Record
+// Delete Category
 Route::delete('/internal/door/categories/{category}', [CategoryController::class, 'destroy'])->name('destroyCategory');
 
-// Get Create Classification View
-Route::get('/internal/door/classifications/create/', [ClassificationController::class, 'create'])->name('createClassification');
+////////////////////////////////
+// Message Routes
+////////////////////////////////
 
-// Store New Classification
-Route::post('/internal/door/classifications', [ClassificationController::class, 'store'])->name('storeClassification');
+// Get All Messages View
+Route::get('/internal/messages', [MessageController::class, 'index']);
 
-// Get Edit Classification View
-Route::get('/internal/door/classifications/{classification}/edit', [ClassificationController::class, 'edit'])->name('editClassification');
+// Store New Message
+Route::post('/internal/messages', [MessageController::class, 'store'])->name('storeMessage');
 
-// Update Classification
-Route::put('/internal/door/classifications/{classification}', [ClassificationController::class, 'update'])->name('updateClassification');
-
-// Destroy Classification
-Route::delete('/internal/door/classifications/{classification}', [ClassificationController::class, 'destroy'])->name('destroyClassification');
+// Get Show Message View
+Route::get('/internal/messages/{listing}', [MessageController::class, 'show']);
