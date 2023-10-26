0b0b8341867f23f8b9bfcc9873c5d975492a1845
Armando Alvear <armando.alvear01@outlook.com>
2023-10-25 19:36:26 -0500

    add categories routes

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,22 @@ Route::get('/internal/messages/{listing}', [MessageController::class, 'show']);
 // Get Categories Index Page (Show all Classifications and their respective categories)
 Route::get('/internal/door/categories', [CategoryController::class, 'index']);
 
-Route::get('/internal/door/categories/create/{classification}', [CategoryController::class, 'create'])->name('create_category');
+// Get Create Category View
+Route::get('/internal/door/categories/create/{classification}', [CategoryController::class, 'create'])->name('createCategory');
+
+// Store New Category
+Route::post('/internal/door/categories', [CategoryController::class, 'store'])->name('storeCategory');
+
+// Get Edit Category View
+Route::get('/internal/door/categories/{category}/edit', [CategoryController::class, 'edit'])->name('editCategory');
+
+// Update Category
+Route::put('/internal/door/categories/{category}', [CategoryController::class, 'update'])->name('updateCategory');
+
+// Delete Category Record
+Route::delete('/internal/door/categories/{category}', [CategoryController::class, 'destroy'])->name('destroyCategory');
+
+// Get Create Classification View
+Route::get('/internal/door/classifications/create/', [ClassificationController::class, 'create'])->name('createClassification');
 
 
