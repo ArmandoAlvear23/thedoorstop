3d211e82b1bd394adbd6e4558d13a22e7deab157
Armando Alvear <armando.alvear01@outlook.com>
2023-10-28 14:20:06 -0500

    modify colors and spacing

--- resources/views/classifications/create.blade.php
+++ resources/views/classifications/create.blade.php
@@ -2,7 +2,7 @@
     <x-padding-wrapper>
         <div class="bg-gray-50 border border-gray-200 border-y-sky-300 border-y-4 rounded p-10 pb-14 my-8 mx-0 md:mx-8 shadow-xl overflow-auto">
             <header class="text-center">
-                <h2 class="text-2xl font-medium uppercase mb-4">
+                <h2 class="text-2xl font-medium uppercase text-gray-700 mb-4">
                     Add Classification
                 </h2>
             </header>
@@ -11,7 +11,7 @@
                 <div class="mb-6">
                     <label
                         for="name"
-                        class="inline-block text-lg mb-2"
+                        class="inline-block text-lg text-gray-800 mb-2"
                         >Classification Name</label
                     >
                     <input
@@ -27,7 +27,7 @@
                 </div>
                 <div class="mb-6 flex flex-col justify-center">
                     <button
-                        class="bg-primary text-white rounded py-2 px-4 hover:bg-secondary"
+                        class="bg-primary text-white rounded py-2 px-4 hover:bg-primary2 transition ease-out duration-200"
                     >
                         Create Classification
                     </button>
