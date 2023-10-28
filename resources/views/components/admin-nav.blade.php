fd84e1e4af33a075691d6b0496fe58cd320db254
Armando Alvear <armando.alvear01@outlook.com>
2023-10-28 03:01:15 -0500

    fix admin nav doors border underline when on doors page

--- resources/views/components/admin-nav.blade.php
+++ resources/views/components/admin-nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="bg-secondary py-1 px-6 hidden sm:block shadow-md">
     <ul class="flex sm:space-x-7 lg:justify-center">
-        <li class="{{ Request::is('/doors') ? 'border-primary' : 'border-transparent' }} border-b-2 hover:border-primary transition ease-out duration-500"><a href="/doors" class="text-white uppercase text-sm">Doors</a></li>
+        <li class="{{ Request::is('doors') ? 'border-primary' : 'border-transparent' }} border-b-2 hover:border-primary transition ease-out duration-500"><a href="/doors" class="text-white uppercase text-sm">Doors</a></li>
         <li class="{{ Request::is('internal/door/categories') ? 'border-primary' : 'border-transparent' }} border-b-2 hover:border-primary transition ease-out duration-500"><a href="/internal/door/categories" class="text-white uppercase text-sm">Categories</a></li>
         <li class="{{ Request::is('internal/messages') ? 'border-primary' : 'border-transparent' }} border-b-2 hover:border-primary transition ease-out duration-500"><a href="/internal/messages" class="text-white uppercase text-sm">Messages</a></li>
     </ul>
