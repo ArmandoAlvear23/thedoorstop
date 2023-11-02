2f52d22fd049e9ce0470ab6c2b78143b8b6de893
Armando Alvear <armando.alvear01@outlook.com>
2023-11-02 17:30:43 -0500

    modify routes and redirects

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,6 +15,8 @@ class UserController extends Controller
 
     // Create New User
     public function store(Request $request) {
+
+        // Validate form input
         $formFields = $request->validate([
             'name' => ['required', 'min:3'],
             'email' => ['required', 'email', Rule::unique('users', 'email')],
@@ -30,18 +32,25 @@ class UserController extends Controller
         // Login
         auth()->login($user);
 
-        return redirect('/')->with('message', 'User created and logged in!');
+        // Redirect to home page
+        return redirect()->route('home')->with('message', 'User created and logged in!');
         
     }
 
     // Logout User
     public function logout(Request $request) {
+
+        // Logout user
         auth()->logout();
 
+        // Invalidate session
         $request->session()->invalidate();
+
+        // Regenerate session token
         $request->session()->regenerateToken();
 
-        return redirect('/')->with('message', 'You have been logged out');
+        // Redirect to home page
+        return redirect()->route('home')->with('message', 'You have been logged out!');
     }
 
     // Show Login Form
@@ -51,17 +60,24 @@ class UserController extends Controller
 
     // Authenticate User
     public function authenticate(Request $request) {
+
+        // Validate login form input
         $formFields = $request->validate([
             'email' => ['required', 'email'],
             'password' => 'required'
         ]);
 
+        // Attempt to login with form inputs
         if(auth()->attempt($formFields)) {
+
+            // Regenerate session
             $request->session()->regenerate();
 
-            return redirect('/')->with('message', 'You are now logged in!');
+            // Redirect to home page
+            return redirect()->route('home')->with('message', 'Welecome '.auth()->user()->name.'!');
         }
 
+        // Redirect back to login form if there is an authentication error
         return back()->withErrors(['email' => 'Invalid Credentials'])->onlyInput('email');
     }
 }
