0c3e88d91b8616bcce8b4a695329810dbf68a502
Armando Alvear <armando.alvear01@outlook.com>
2023-11-02 17:00:34 -0500

    modify return redirects and status messages

--- app/Http/Controllers/ClassificationController.php
+++ app/Http/Controllers/ClassificationController.php
@@ -9,19 +9,24 @@ class ClassificationController extends Controller
 {
     // Create category view
     public function create() {
+
+        // Return category create view with data
         return view('classifications.create');
     }
 
     // Store classification
     public function store(Request $request) {
         
+        // Validate form input
         $formFields = $request->validate([
             'name' => 'required'
         ]);
 
-        Classification::create($formFields);
+        // store classification to database
+        $classification = Classification::create($formFields);
 
-        return redirect('internal/door/categories')->with('message', 'Classification created successfully!');
+        // Redirect to category index page
+        return redirect()->route('indexCategory')->with('message', $classification->name.' classification created successfully!');
     }
 
     // Edit Classification
@@ -32,24 +37,33 @@ class ClassificationController extends Controller
     // Update Classification
     public function update(Request $request, Classification $classification) {
         
+        // Validate form input
         $formFields = $request->validate([
             'name' => 'required'
         ]);
 
+        // Update classification
         $classification->update($formFields);
 
-        return redirect('/internal/door/categories')->with('message', 'Classification updated successfully!');
+        // Redirect to category index page
+        return redirect()->route('indexCategory')->with('message', $classification->name.' classification updated successfully!');
     }
 
     // Destroy Classification
     public function destroy(Classification $classification) {
         
+        // Check to make sure there are no categories with this classification
         if(count($classification->categories) != 0) {
             abort(403, 'Unauthorized Action: There are ' . count($classification->categories) . ' doors attached to this category');
         }
 
+        // Get classification name
+        $classificationName = $classification->name;
+        
+        // Delete classification from database
         $classification->delete();
         
-        return redirect('/internal/door/categories')->with('message', 'Classification deleted successfully!');
+        // Redirect to category index page
+        return redirect()->route('indexCategory')->with('message', $classificationName.' classification deleted successfully!');
     }
 }
