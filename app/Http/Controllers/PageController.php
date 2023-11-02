a47a8fb94108853fec790cfb326b6c6365618d95
Armando Alvear <armando.alvear01@outlook.com>
2023-11-01 22:10:03 -0500

    add home doors to home page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,13 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Door;
 use Illuminate\Http\Request;
+use App\Models\Classification;
 
 class PageController extends Controller
 {
-    //
+    // Get home data and page view
     public function index() {
+        
+        // Get all doors with home promotion
+        $homeDoors = Door::query()->with(['promotions', 'categories'])->whereHas('promotions', fn($query) => $query->where('name', 'home'))->get();
+        
+        // Get 14 random doors if there are no home doors
+        if(count($homeDoors) == 0)
+        {
+            $homeDoors = Door::query()->inRandomOrder()->limit(14)->get();
+        }
+
         return view('pages.index', [
-            'heroImages' => Door::query()->with('promotions')->whereHas('promotions', fn($query) => $query->where('name', 'hero'))->get(['img_location'])
+            'heroImages' => Door::query()->with('promotions')->whereHas('promotions', fn($query) => $query->where('name', 'hero'))->get(['img_location']),
+            'homeDoors' => $homeDoors,
+            'classifications' => Classification::query()->with(['categories', 'categories' => fn($q) => $q->withCount('doors')->orderBy('doors_count', 'DESC')])->orderBy('id','ASC')->get(),
+            'activeFilters' => array()
         ]);
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,3 +1,18 @@
 <x-layout>
     <x-hero :heroImages="$heroImages"></x-hero>
+    <x-padding-wrapper>
+        <section class="mt-8">
+            @include('partials._search-doors')
+            <x-door-filter :classifications="$classifications" :activeFilters="$activeFilters" />
+            <div class="lg:grid xl:grid-cols-2 gap-4 space-y-4 md:space-y-0 my-6">
+                @unless(count($homeDoors) == 0)
+                @foreach($homeDoors as $door)
+                    <x-door-card :door="$door" :activeFilters="$activeFilters" />
+                @endforeach
+                @else
+                    <p>No doors found</p>
+                @endunless
+            </div>
+        </section>
+    </x-padding-wrapper>
 </x-layout>
