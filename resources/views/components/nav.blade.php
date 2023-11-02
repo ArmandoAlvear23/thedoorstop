cbaed423a3dcea1c2a8768b18de3e28275d33fad
Armando Alvear <armando.alvear01@outlook.com>
2023-11-02 02:09:29 -0500

    add door style nav links to category filter

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,3 +1,4 @@
+@props(['categories'])
 <header>
     <div 
         x-data="{ open: false }"
@@ -28,19 +29,29 @@
                 </div>
             </div>
         </div>
+        @php
+            $modernId = 3;
+            $traditionalId = 4;
+            $rusticId = 5;
+            $craftsmanId = 6;
+        @endphp
         <!-- Desktop Navbar -->
         <nav class="bg-primary py-4 px-6 hidden sm:block shadow-md">
             <ul class="flex sm:space-x-7 lg:justify-center">
-                <li class="{{ str_contains(Request::getRequestUri(), '?search=modern') ? 'border-secondary' : 'border-transparent' }} border-b-2 hover:border-secondary transition ease-out duration-500"><a href="/doors?search=modern" class="text-white uppercase">Modern</a></li>
-                <li class="{{ str_contains(Request::getRequestUri(), '?search=traditional') ? 'border-secondary' : 'border-transparent' }} border-b-2 hover:border-secondary transition ease-out duration-500"><a href="/doors?search=traditional" class="text-white uppercase">Traditional</a></li>
-                <li class="{{ str_contains(Request::getRequestUri(), '?search=rustic') ? 'border-secondary' : 'border-transparent' }} border-b-2 hover:border-secondary transition ease-out duration-500"><a href="/doors?search=rustic" class="text-white uppercase">Rustic</a></li>
-                <li class="{{ str_contains(Request::getRequestUri(), '?search=craftsman') ? 'border-secondary' : 'border-transparent' }} border-b-2 hover:border-secondary transition ease-out duration-500"><a href="doors?search=craftsman" class="text-white uppercase">Craftsman</a></li>
+                <li class="{{ str_contains(Request::getRequestUri(), '?category%5B%5D='.$modernId) ? 'border-secondary' : 'border-transparent' }} border-b-2 hover:border-secondary transition ease-out duration-500"><a href="/doors?category%5B%5D={{$modernId}}" class="text-white uppercase">Modern</a></li>
+                <li class="{{ str_contains(Request::getRequestUri(), '?category%5B%5D='.$traditionalId) ? 'border-secondary' : 'border-transparent' }} border-b-2 hover:border-secondary transition ease-out duration-500"><a href="/doors?category%5B%5D={{ $traditionalId }}" class="text-white uppercase">Traditional</a></li>
+                <li class="{{ str_contains(Request::getRequestUri(), '?category%5B%5D='.$rusticId) ? 'border-secondary' : 'border-transparent' }} border-b-2 hover:border-secondary transition ease-out duration-500"><a href="/doors?category%5B%5D={{ $rusticId }}" class="text-white uppercase">Rustic</a></li>
+                <li class="{{ str_contains(Request::getRequestUri(), '?category%5B%5D='.$craftsmanId) ? 'border-secondary' : 'border-transparent' }} border-b-2 hover:border-secondary transition ease-out duration-500"><a href="doors?category%5B%5D={{ $craftsmanId }}" class="text-white uppercase">Craftsman</a></li>
                 <li class="{{ Request::is('about') ? 'border-secondary' : 'border-transparent' }} border-b-2 hover:border-secondary transition ease-out duration-500"><a href="/about" class="text-white uppercase">About</a></li>
                 <li class="{{ Request::is('contact') ? 'border-secondary' : 'border-transparent' }} border-b-2 hover:border-secondary transition ease-out duration-500"><a href="/contact" class="text-white uppercase">Contact</a></li>
             </ul>
         </nav>
         @auth
-            @include('components.admin-nav')
+            <nav class="bg-gray-600 py-1 px-6 hidden sm:block shadow-md">
+                <ul class="flex sm:space-x-7 lg:justify-center">
+                    @include('partials._admin-links')
+                </ul>
+            </nav>
         @endauth
         
         <!-- Mobile Nav dropdown -->
@@ -53,16 +64,16 @@
         >
             <div class="bg-primary w-full h-screen">
                 <ul class="flex justify-center flex-col items-start px-8 text-4xl">
-                    <a href="/doors?search=modern" class="my-2 w-full bg-primary {{ str_contains(Request::getRequestUri(), '?search=modern') ? 'text-secondary border-secondary' : 'text-white border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
+                    <a href="/doors?category%5B%5D={{ $modernId }}" class="my-2 w-full bg-primary {{ str_contains(Request::getRequestUri(), '?category%5B%5D='.$modernId) ? 'text-secondary border-secondary' : 'text-white border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
                         <li class="pb-2">Modern</li>
                     </a>
-                    <a href="/doors?search=traditional" class="my-2 w-full bg-primary {{ str_contains(Request::getRequestUri(), '?search=traditional') ? 'text-secondary border-secondary' : 'text-white first-letter:border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
+                    <a href="/doors?category%5B%5D={{ $traditionalId }}" class="my-2 w-full bg-primary {{ str_contains(Request::getRequestUri(), '?category%5B%5D='.$traditionalId) ? 'text-secondary border-secondary' : 'text-white first-letter:border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
                         <li class="pb-2">Traditional</li>
                     </a>
-                    <a href="/doors?search=rustic" class="my-2 w-full bg-primary {{ str_contains(Request::getRequestUri(), '?search=rustic') ? 'text-secondary border-secondary' : 'text-white border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
+                    <a href="/doors?category%5B%5D={{ $rusticId }}" class="my-2 w-full bg-primary {{ str_contains(Request::getRequestUri(), '?category%5B%5D='.$rusticId) ? 'text-secondary border-secondary' : 'text-white border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
                         <li class="pb-2">Rustic</li>
                     </a>
-                    <a href="/doors?search=craftsman" class="my-2 w-full bg-primary {{ str_contains(Request::getRequestUri(), '?search=craftsman') ? 'text-secondary border-secondary' : 'text-white border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
+                    <a href="/doors?category%5B%5D={{ $craftsmanId }}" class="my-2 w-full bg-primary {{ str_contains(Request::getRequestUri(), '?category%5B%5D='.$craftsmanId) ? 'text-secondary border-secondary' : 'text-white border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
                         <li class="pb-2">Craftsman</li>
                     </a>
                     <a href="/about" class="my-2 w-full bg-primary {{ Request::is('about') ? 'text-secondary border-secondary' : 'text-white border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
@@ -71,6 +82,35 @@
                     <a href="/contact" class="my-2 w-full bg-primary {{ Request::is('contact') ? 'text-secondary border-secondary' : 'text-white border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
                         <li class="pb-2">Contact Us</li>
                     </a>
+                    @auth
+                        <a href="/doors" class="my-2 w-full bg-primary {{ Request::is('doors') ? 'text-secondary border-secondary' : 'text-white border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
+                            <li class="pb-2">Doors (Admin)</li>
+                        </a>
+
+                        <a href="/internal/doors/create" class="my-2 w-full bg-primary {{ Request::is('internal/doors/create') ? 'text-secondary border-secondary' : 'text-white border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
+                            <li class="pb-2">Upload Door (Admin)</li>
+                        </a>
+
+                        <a href="internal/door/categories" class="my-2 w-full bg-primary {{ Request::is('internal/door/categories') ? 'text-secondary border-secondary' : 'text-white border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
+                            <li class="pb-2">Categories (Admin)</li>
+                        </a>
+                        <a href="internal/messages" class="my-2 w-full bg-primary {{ Request::is('internal/messages') ? 'text-secondary border-secondary' : 'text-white border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
+                            <li class="pb-2">Messages (Admin)</li>
+                        </a>
+
+                        <a href="internal/messages" class="my-2 w-full bg-primary {{ Request::is('internal/messages') ? 'text-secondary border-secondary' : 'text-white border-gray-300' }} border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
+                            <li class="pb-2">Messages (Admin)</li>
+                        </a>
+
+                    <li class="my-2 w-full bg-primary border-b-2 border-dashed hover:text-secondary hover:border-secondary transition ease-out duration-500">
+                        <form method="POST" action="{{ route('logoutUser') }}" class="pb-2">
+                            @csrf
+                            <button type="submit" class="text-white hover:text-secondary hover:border-secondary">
+                                <i class="fa-solid fa-door-closed"></i> Logout (Admin)
+                            </button>
+                        </form>
+                    </li>
+                    @endauth
                 </ul>
             </div>
         </nav>
---
+++ resources/views/partials/_admin-links.blade.php
@@ -0,0 +1,12 @@
+<li class="{{ Request::is('doors') ? 'border-white' : 'border-transparent' }} border-b-2 hover:border-white transition ease-out duration-500"><a href="/doors" class="text-white uppercase text-sm">Doors</a></li>
+<li class="{{ Request::is('internal/doors/create') ? 'border-white' : 'border-transparent' }} border-b-2 hover:border-white transition ease-out duration-500"><a href="/internal/doors/create" class="text-white uppercase text-sm">Upload Door</a></li>
+<li class="{{ Request::is('internal/door/categories') ? 'border-white' : 'border-transparent' }} border-b-2 hover:border-white transition ease-out duration-500"><a href="/internal/door/categories" class="text-white uppercase text-sm">Categories</a></li>
+<li class="{{ Request::is('internal/messages') ? 'border-white' : 'border-transparent' }} border-b-2 hover:border-white transition ease-out duration-500"><a href="/internal/messages" class="text-white uppercase text-sm">Messages</a></li>
+<li>
+    <form method="POST" action="{{ route('logoutUser') }}" class="inline">
+        @csrf
+        <button type="submit" class="text-white">
+            <i class="fa-solid fa-door-closed"></i> Logout
+        </button>
+    </form>
+</li>
