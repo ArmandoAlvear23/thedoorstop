5651ae390dc03be5294b0d1667a9a077034f0433
Armando Alvear <armando.alvear01@outlook.com>
2023-11-03 16:12:10 -0500

    add testimonial controller functions

--- app/Http/Controllers/TestimonialController.php
+++ app/Http/Controllers/TestimonialController.php
@@ -7,11 +7,64 @@ use Illuminate\Http\Request;
 
 class TestimonialController extends Controller
 {
-    // Show all testimonials
+    // Get and show all testimonials
     public function index() {
 
         return view('testimonials.index', [
             'testimonials' => Testimonial::latest()->paginate(25)
         ]);
+    }
+
+    // Get create testimonial view
+    public function create() {
+        return view('testimonials.create');
+    }
+
+    // Store testimonial
+    public function store(Request $request) {
+        
+        // Validate form input
+        $formFields = $request->validate([
+            'name' => 'required',
+            'testimonial' => 'required'
+        ]);
+
+        // Store testimonial to database
+        Testimonial::create($formFields);
+
+        // Redirect to testimonials index page
+        return redirect()->route('indexTestimonial')->with('message', 'Testimonial added successfully!');
+    }
+
+    // Edit testimonial
+    public function edit(Testimonial $testimonial) {
+
+        return view('testimonials.edit', ['testimonial' => $testimonial]);
+    }
+
+    // Update testimonial
+    public function update(Request $request, Testimonial $testimonial) {
+        
+        // Validate form input
+        $formFields = $request->validate([
+            'name' => 'required',
+            'testimonial' => 'required'
+        ]);
+
+        // Update testimonial in database
+        $testimonial->update($formFields);
+
+        // Redirect to testimonials index page
+        return redirect()->route('indexTestimonial')->with('message', 'Testimonial updated successfully!');
+    }
+
+    // Delete testimonial
+    public function destroy(Testimonial $testimonial) {
+        
+        // Delete testimonial from database
+        $testimonial->delete();
+
+        // Redirect to testimonials index page
+        return redirect()->route('indexTestimonial')->with('message', 'Testimonial deleted successfully!');
     }
 }
