19abe80ed63845e7b979d5a357707f63b0fd294b
Armando Alvear <armando.alvear01@outlook.com>
2023-11-13 09:57:40 -0600

    add promotions seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\Door;
 use App\Models\Message;
 use App\Models\Category;
 use App\Models\Classification;
+use App\Models\Promotion;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,8 +19,17 @@ class DatabaseSeeder extends Seeder
     {
         //\App\Models\User::factory(5)->create();
 
+        //Create Promotion records
+        Promotion::create([
+            'name' => 'hero'
+        ]);
+
+        Promotion::created([
+            'name' => 'home'
+        ]);
+
         // Create test message
-        Message::factory(50)->create();
+        Message::factory(10)->create();
 
         // Create category classifications
         Classification::create([
@@ -76,28 +86,22 @@ class DatabaseSeeder extends Seeder
         Door::create([
             'name' => 'Modern Door 1',
             'sku' => 'moderndoor1',
-            'img_location' => 'doors/yaasZgAjWUgCxOvWRzXOmWYFpSENPQHfzkPkYVwd.png',
+            'img_location' => 'storage/doors/yaasZgAjWUgCxOvWRzXOmWYFpSENPQHfzkPkYVwd.png',
         ]);
         Door::create([
             'name' => 'Traditional Door 1',
             'sku' => 'traddoor1',
-            'img_location' => 'doors/hcozZednoKvGmwpMTGOkHNenRfLRmcXbYJOLfdZW.png',
+            'img_location' => 'storage/doors/hcozZednoKvGmwpMTGOkHNenRfLRmcXbYJOLfdZW.png',
         ]);
         Door::create([
             'name' => 'Rustic Door 1',
             'sku' => 'rusticdoor1',
-            'img_location' => 'doors/IueMURBmqrIvJQvpDzjOrfZLIOzQnYCrUYvRLxmX.png',
+            'img_location' => 'storage/doors/IueMURBmqrIvJQvpDzjOrfZLIOzQnYCrUYvRLxmX.png',
         ]);
         Door::create([
             'name' => 'Craftsman Door 1',
             'sku' => 'craftdoor1',
-            'img_location' => 'doors/ZHTOzbzQLNFzBXWJPkFOAqOlZmKhUGCojQfDkFVG.png',
+            'img_location' => 'storage/doors/ZHTOzbzQLNFzBXWJPkFOAqOlZmKhUGCojQfDkFVG.png',
         ]);
-
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
