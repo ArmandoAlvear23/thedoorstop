138a386baf664b22edf844117ed546035c552f43
Armando Alvear <armando.alvear01@outlook.com>
2023-11-14 15:40:50 -0600

    randomize testimonials order

--- app/Http/Controllers/TestimonialController.php
+++ app/Http/Controllers/TestimonialController.php
@@ -11,7 +11,7 @@ class TestimonialController extends Controller
     public function index() {
 
         return view('testimonials.index', [
-            'testimonials' => Testimonial::latest()->paginate(25)
+            'testimonials' => Testimonial::query()->inRandomOrder()->get()
         ]);
     }
 
